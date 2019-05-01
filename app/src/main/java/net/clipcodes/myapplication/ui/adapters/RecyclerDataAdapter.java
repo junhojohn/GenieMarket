8d5c8fddc3399bcc44b6c674160aa43a47c0406c
junhojohn7 <junhojohn@gmail.com>
2019-05-01 20:19:55 +0900

    modify category pages.

--- app/src/main/java/net/clipcodes/myapplication/ui/adapters/RecyclerDataAdapter.java
+++ app/src/main/java/net/clipcodes/myapplication/ui/adapters/RecyclerDataAdapter.java
@@ -7,6 +7,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,6 +34,26 @@ public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapte
     @Override
     public void onBindViewHolder(RecyclerDataAdapter.MyViewHolder holder, int position) {
         DummyParentDataItem dummyParentDataItem = dummyParentDataItems.get(position);
+        switch (position){
+            case 0:
+                holder.img_category.setImageResource(R.drawable.ic_category_clothes);
+                break;
+            case 1:
+                holder.img_category.setImageResource(R.drawable.ic_category_shoes);
+                break;
+            case 2:
+                holder.img_category.setImageResource(R.drawable.ic_category_bags);
+                break;
+            case 3:
+                holder.img_category.setImageResource(R.drawable.ic_category_accessories);
+                break;
+            case 4:
+                holder.img_category.setImageResource(R.drawable.ic_category_toys);
+                break;
+            case 5:
+                holder.img_category.setImageResource(R.drawable.ic_category_electronics);
+                break;
+        }
         holder.textView_parentName.setText(dummyParentDataItem.getParentName());
         //
         int noOfChildTextViews = holder.linearLayout_childItems.getChildCount();
@@ -68,12 +89,16 @@ public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapte
     class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Context context;
         private TextView textView_parentName;
+        private ImageView img_category;
+        private ImageView img_arrow;
         private LinearLayout linearLayout_childItems;
 
         MyViewHolder(View itemView) {
             super(itemView);
             context = itemView.getContext();
-            textView_parentName = itemView.findViewById(R.id.tv_parentName);
+            textView_parentName = itemView.findViewById(R.id.tv_bigCategoryName);
+            img_category = itemView.findViewById(R.id.img_category);
+            img_arrow = itemView.findViewById(R.id.img_arrow);
             linearLayout_childItems = itemView.findViewById(R.id.ll_child_items);
             linearLayout_childItems.setVisibility(View.GONE);
             int intMaxNoOfChild = 0;
@@ -86,17 +111,22 @@ public class RecyclerDataAdapter extends RecyclerView.Adapter<RecyclerDataAdapte
                 textView.setId(indexView);
                 textView.setPadding(0, 20, 0, 20);
                 textView.setGravity(Gravity.CENTER);
+                textView.setTextSize(25);
                 textView.setBackground(ContextCompat.getDrawable(context, R.drawable.background_sub_module_text));
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
                 textView.setOnClickListener(this);
                 linearLayout_childItems.addView(textView, layoutParams);
             }
+            img_category.setOnClickListener(this);
             textView_parentName.setOnClickListener(this);
+            img_arrow.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View view) {
-            if (view.getId() == R.id.tv_parentName) {
+            if (view.getId() == R.id.tv_bigCategoryName ||
+                    view.getId() == R.id.img_category ||
+                    view.getId() == R.id.img_arrow) {
                 if (linearLayout_childItems.getVisibility() == View.VISIBLE) {
                     linearLayout_childItems.setVisibility(View.GONE);
                 } else {
--- app/src/main/java/net/clipcodes/myapplication/ui/pages/PageCategory.java
+++ app/src/main/java/net/clipcodes/myapplication/ui/pages/PageCategory.java
@@ -36,117 +36,50 @@ public class PageCategory extends Fragment {
         ArrayList<DummyChildDataItem> childDataItems;
         /////////
         childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("A Child 1"));
-        childDataItems.add(new DummyChildDataItem("A Child 2"));
-        childDataItems.add(new DummyChildDataItem("A Child 3"));
-        arrDummyData.add(new DummyParentDataItem("A Parent, 3 Children", childDataItems));
+        childDataItems.add(new DummyChildDataItem("상의"));
+        childDataItems.add(new DummyChildDataItem("하의"));
+        childDataItems.add(new DummyChildDataItem("코트"));
+        childDataItems.add(new DummyChildDataItem("모자"));
+        childDataItems.add(new DummyChildDataItem("양말"));
+        childDataItems.add(new DummyChildDataItem("장갑"));
+        childDataItems.add(new DummyChildDataItem("기타의류"));
+        arrDummyData.add(new DummyParentDataItem("의류", childDataItems));
         /////////
         childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("B Child 1"));
-        childDataItems.add(new DummyChildDataItem("B Child 2"));
-        childDataItems.add(new DummyChildDataItem("B Child 3"));
-        childDataItems.add(new DummyChildDataItem("B Child 4"));
-        childDataItems.add(new DummyChildDataItem("B Child 5"));
-        childDataItems.add(new DummyChildDataItem("B Child 6"));
-        arrDummyData.add(new DummyParentDataItem("B Parent, 6 Children", childDataItems));
+        childDataItems.add(new DummyChildDataItem("구두"));
+        childDataItems.add(new DummyChildDataItem("워커"));
+        childDataItems.add(new DummyChildDataItem("단화"));
+        childDataItems.add(new DummyChildDataItem("운동화"));
+        childDataItems.add(new DummyChildDataItem("슬리퍼"));
+        childDataItems.add(new DummyChildDataItem("어린이신발"));
+        childDataItems.add(new DummyChildDataItem("기타신발"));
+        arrDummyData.add(new DummyParentDataItem("신발", childDataItems));
         /////////
         childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("C Child 1"));
-        childDataItems.add(new DummyChildDataItem("C Child 2"));
-        childDataItems.add(new DummyChildDataItem("C Child 3"));
-        childDataItems.add(new DummyChildDataItem("C Child 4"));
-        childDataItems.add(new DummyChildDataItem("C Child 5"));
-        childDataItems.add(new DummyChildDataItem("C Child 6"));
-        childDataItems.add(new DummyChildDataItem("C Child 7"));
-        childDataItems.add(new DummyChildDataItem("C Child 8"));
-        childDataItems.add(new DummyChildDataItem("C Child 9"));
-        arrDummyData.add(new DummyParentDataItem("C Parent, 9 Children", childDataItems));
+        childDataItems.add(new DummyChildDataItem("토드백"));
+        childDataItems.add(new DummyChildDataItem("지갑"));
+        childDataItems.add(new DummyChildDataItem("클러치백"));
+        childDataItems.add(new DummyChildDataItem("백팩"));
+        childDataItems.add(new DummyChildDataItem("동전지갑"));
+        arrDummyData.add(new DummyParentDataItem("잡화", childDataItems));
         /////////
         childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("D Child 1"));
-        arrDummyData.add(new DummyParentDataItem("D Parent, 1 Children", childDataItems));
+        childDataItems.add(new DummyChildDataItem("손목시계"));
+        childDataItems.add(new DummyChildDataItem("팔찌"));
+        childDataItems.add(new DummyChildDataItem("기타악세서리"));
+        arrDummyData.add(new DummyParentDataItem("악세서리", childDataItems));
         /////////
         childDataItems = new ArrayList<>();
-        arrDummyData.add(new DummyParentDataItem("E Parent, 0 Children", childDataItems));
+        childDataItems.add(new DummyChildDataItem("인형"));
+        childDataItems.add(new DummyChildDataItem("블럭"));
+        childDataItems.add(new DummyChildDataItem("기타장난감"));
+        arrDummyData.add(new DummyParentDataItem("장난감", childDataItems));
         /////////
         childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("F Child 1"));
-        childDataItems.add(new DummyChildDataItem("F Child 2"));
-        arrDummyData.add(new DummyParentDataItem("F Parent, 2 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("G Child 1"));
-        childDataItems.add(new DummyChildDataItem("G Child 2"));
-        childDataItems.add(new DummyChildDataItem("G Child 3"));
-        childDataItems.add(new DummyChildDataItem("G Child 4"));
-        arrDummyData.add(new DummyParentDataItem("G Parent, 4 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("H Child 1"));
-        childDataItems.add(new DummyChildDataItem("H Child 2"));
-        childDataItems.add(new DummyChildDataItem("H Child 3"));
-        childDataItems.add(new DummyChildDataItem("H Child 4"));
-        childDataItems.add(new DummyChildDataItem("H Child 5"));
-        childDataItems.add(new DummyChildDataItem("H Child 6"));
-        childDataItems.add(new DummyChildDataItem("H Child 7"));
-        childDataItems.add(new DummyChildDataItem("H Child 8"));
-        arrDummyData.add(new DummyParentDataItem("H Parent, 8 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("I Child 1"));
-        arrDummyData.add(new DummyParentDataItem("I Parent, 1 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("J Child 1"));
-        childDataItems.add(new DummyChildDataItem("J Child 2"));
-        childDataItems.add(new DummyChildDataItem("J Child 3"));
-        childDataItems.add(new DummyChildDataItem("J Child 4"));
-        childDataItems.add(new DummyChildDataItem("J Child 5"));
-        arrDummyData.add(new DummyParentDataItem("J Parent, 5 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("K Child 1"));
-        childDataItems.add(new DummyChildDataItem("K Child 2"));
-        childDataItems.add(new DummyChildDataItem("K Child 3"));
-        childDataItems.add(new DummyChildDataItem("K Child 4"));
-        childDataItems.add(new DummyChildDataItem("K Child 5"));
-        childDataItems.add(new DummyChildDataItem("K Child 6"));
-        childDataItems.add(new DummyChildDataItem("K Child 7"));
-        arrDummyData.add(new DummyParentDataItem("K Parent, 7 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("L Child 1"));
-        childDataItems.add(new DummyChildDataItem("L Child 2"));
-        arrDummyData.add(new DummyParentDataItem("L Parent, 2 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("M Child 1"));
-        childDataItems.add(new DummyChildDataItem("M Child 2"));
-        childDataItems.add(new DummyChildDataItem("M Child 3"));
-        childDataItems.add(new DummyChildDataItem("M Child 4"));
-        childDataItems.add(new DummyChildDataItem("M Child 5"));
-        arrDummyData.add(new DummyParentDataItem("M Parent, 5 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("N Child 1"));
-        childDataItems.add(new DummyChildDataItem("N Child 2"));
-        childDataItems.add(new DummyChildDataItem("N Child 3"));
-        childDataItems.add(new DummyChildDataItem("N Child 4"));
-        childDataItems.add(new DummyChildDataItem("N Child 5"));
-        childDataItems.add(new DummyChildDataItem("N Child 6"));
-        childDataItems.add(new DummyChildDataItem("N Child 7"));
-        childDataItems.add(new DummyChildDataItem("N Child 8"));
-        childDataItems.add(new DummyChildDataItem("N Child 9"));
-        childDataItems.add(new DummyChildDataItem("N Child 10"));
-        childDataItems.add(new DummyChildDataItem("N Child 11"));
-        childDataItems.add(new DummyChildDataItem("N Child 12"));
-        arrDummyData.add(new DummyParentDataItem("N Parent, 12 Children", childDataItems));
-        /////////
-        childDataItems = new ArrayList<>();
-        childDataItems.add(new DummyChildDataItem("O Child 1"));
-        childDataItems.add(new DummyChildDataItem("O Child 2"));
-        arrDummyData.add(new DummyParentDataItem("O Parent, 2 Children", childDataItems));
-        ////////
+        childDataItems.add(new DummyChildDataItem("휴대폰주변기기"));
+        childDataItems.add(new DummyChildDataItem("기타전자제품"));
+        arrDummyData.add(new DummyParentDataItem("전자제품", childDataItems));
+
         return arrDummyData;
     }
 }
