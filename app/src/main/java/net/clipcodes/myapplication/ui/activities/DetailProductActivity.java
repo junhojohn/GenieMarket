a78b0b6f42f6afaeb2fb02b7b689f878731a734f
junhojohn7 <junhojohn@gmail.com>
2019-05-06 16:24:24 +0900

    Add image slider auto timer. Change background color of all activities and fragments. Add Search Toolbars.(under implementing)

--- app/src/main/java/net/clipcodes/myapplication/ui/activities/DetailProductActivity.java
+++ app/src/main/java/net/clipcodes/myapplication/ui/activities/DetailProductActivity.java
@@ -11,12 +11,16 @@ import net.clipcodes.myapplication.R;
 import net.clipcodes.myapplication.ui.adapters.ProductImageSliderAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DetailProductActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout indicator;
     ImageView mFlower;
     TextView mDescription;
+    List<Integer> imageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +36,13 @@ public class DetailProductActivity extends AppCompatActivity {
         ProductImageSliderAdapter adapter = new ProductImageSliderAdapter(this);
         Bundle mBundle = getIntent().getExtras();
         if (mBundle != null) {
-            adapter.setImageList(mBundle.getIntegerArrayList("ImageList"));
+            imageList = mBundle.getIntegerArrayList("ImageList");
+            adapter.setImageList(imageList);
             viewPager.setAdapter(adapter);
             mDescription.setText(mBundle.getString("Description") + "\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na");
+
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new SliderTimer(), 4000, 6000);
         }
     }
 
@@ -55,4 +63,21 @@ public class DetailProductActivity extends AppCompatActivity {
 //            mDescription.setText(mBundle.getString("Description"));
 //        }
 //    }
+
+    private class SliderTimer extends TimerTask {
+
+        @Override
+        public void run() {
+            DetailProductActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (viewPager.getCurrentItem() < imageList.size() - 1) {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                    } else {
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
+    }
 }
