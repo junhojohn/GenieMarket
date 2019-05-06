4bcfe81817d9c90a1550810f82fbc8820900d0cb
junhojohn7 <junhojohn@gmail.com>
2019-05-06 15:39:32 +0900

    Improve Detail Product Activity UX. Add Purchase Empty Activity.

--- app/src/main/java/net/clipcodes/myapplication/ui/activities/DetailProductActivity.java
+++ app/src/main/java/net/clipcodes/myapplication/ui/activities/DetailProductActivity.java
@@ -1,37 +1,44 @@
 package net.clipcodes.myapplication.ui.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import net.clipcodes.myapplication.R;
+import net.clipcodes.myapplication.ui.adapters.ProductImageSliderAdapter;
 
 import java.util.ArrayList;
 
 public class DetailProductActivity extends AppCompatActivity {
-
+    ViewPager viewPager;
+    TabLayout indicator;
     ImageView mFlower;
     TextView mDescription;
-    ArrayList<Integer> imageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        viewPager = findViewById(R.id.product_image_slider);
+        indicator = findViewById(R.id.product_image_indicator);
+        indicator.setupWithViewPager(viewPager, true);
         mFlower = findViewById(R.id.ivImage);
         mDescription = findViewById(R.id.tvDescription);
 
+        ProductImageSliderAdapter adapter = new ProductImageSliderAdapter(this);
         Bundle mBundle = getIntent().getExtras();
         if (mBundle != null) {
-//            mFlower.setImageResource(mBundle.getInt("Image"));
-            imageList = mBundle.getIntegerArrayList("ImageList");
-            mFlower.setImageResource(imageList.get(0));
+            adapter.setImageList(mBundle.getIntegerArrayList("ImageList"));
+            viewPager.setAdapter(adapter);
             mDescription.setText(mBundle.getString("Description") + "\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na");
         }
     }
 
+    // previouse code.
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
---
+++ app/src/main/java/net/clipcodes/myapplication/ui/adapters/ProductImageSliderAdapter.java
@@ -0,0 +1,58 @@
+package net.clipcodes.myapplication.ui.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import net.clipcodes.myapplication.R;
+
+import java.util.List;
+
+public class ProductImageSliderAdapter extends PagerAdapter {
+    private ImageView imageView;
+    private Context context;
+    private List<Integer> imageList;
+
+    public ProductImageSliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imageList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.item_product_image_slider, null);
+
+        imageView = view.findViewById(R.id.ivImage);
+        imageView.setImageResource(this.imageList.get(position));
+
+        ViewPager viewPager = (ViewPager) container;
+        viewPager.addView(view, 0);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        ViewPager viewPager = (ViewPager) container;
+        View view = (View) object;
+        viewPager.removeView(view);
+    }
+
+    public void setImageList(List<Integer> imageList){
+        this.imageList = imageList;
+    }
+}
