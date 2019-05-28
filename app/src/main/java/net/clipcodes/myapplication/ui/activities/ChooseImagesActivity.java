453011fcc4ba1cd1764396daef108235febc8a98
junhojohn7 <junhojohn@gmail.com>
2019-05-28 23:03:22 +0900

    add choose image page.

--- app/src/main/java/net/clipcodes/myapplication/ui/activities/ChooseImagesActivity.java
+++ app/src/main/java/net/clipcodes/myapplication/ui/activities/ChooseImagesActivity.java
@@ -1,17 +1,43 @@
 package net.clipcodes.myapplication.ui.activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.travijuu.numberpicker.library.NumberPicker;
 
 import net.clipcodes.myapplication.R;
+import net.clipcodes.myapplication.models.Picture;
+import net.clipcodes.myapplication.ui.adapters.GalleryItemAdapter;
+import net.clipcodes.myapplication.utils.ConstantDataManager;
+import net.clipcodes.myapplication.utils.Libraries;
+
+import java.util.ArrayList;
 
 public class ChooseImagesActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Picture> pictures;
+    GalleryItemAdapter adapter;
+    Handler handler;
+
+    private ImageView imageViewButtonSend;
+    private TextView textViewSelectedCount;
+    private ConstraintLayout constraintLayout;
 
     Toolbar toolbar;
     Button btnFinish;
@@ -32,6 +58,84 @@ public class ChooseImagesActivity extends AppCompatActivity {
         btnFinish.setOnClickListener(clik);
         btnBack.setOnClickListener(clik);
 
+        imageViewButtonSend = findViewById(R.id.button_send);
+        textViewSelectedCount = findViewById(R.id.textViewSeletedCount);
+        constraintLayout = findViewById(R.id.layoutSend);
+
+        imageViewButtonSend.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        pictures = new ArrayList<Picture>();
+        recyclerView = findViewById(R.id.recyclerViewGallery);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        adapter = new GalleryItemAdapter(this, pictures, new GalleryItemAdapter.ItemSelectedChangeListener() {
+            @Override
+            public void onItemSelectedChange(int number) {
+                if(number > 0)     {
+                    constraintLayout.setVisibility(View.VISIBLE);
+                    textViewSelectedCount.setText(number+"");
+                }else{
+                    constraintLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+        recyclerView.setAdapter(adapter);
+
+        handler = new Handler();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(ChooseImagesActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            // request permission runtime
+            Libraries.requestPermissionStorage(ChooseImagesActivity.this);
+        }else{
+            // permission allow
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Looper.prepare();
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            pictures.clear();
+                            pictures.addAll(Picture.getGalleryPhotos(ChooseImagesActivity.this));
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                    Looper.loop();
+                }
+            }).start();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == ConstantDataManager.PERMISSION_REQUEST_CODE_EXTERNAL_STORAGE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Looper.prepare();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                pictures.clear();
+                                pictures.addAll(Picture.getGalleryPhotos(ChooseImagesActivity.this));
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                        Looper.loop();
+                    }
+                }).start();
+            }else{
+                Libraries.requestPermissionStorageDeny(ChooseImagesActivity.this);
+            }
+        }
     }
 
     public View.OnClickListener clik = new View.OnClickListener() {
